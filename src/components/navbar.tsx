581f5319f483406a1e56fba11081cecd6a3d319c
felipe <felipe.a.obregon@gmail.com>
2023-05-25 15:30:39 -0400

    move padding

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,12 +2,12 @@ export default function Navbar() {
     const data = Array(10).fill('test');
 
     return (
-        <nav className="flex flex-col items-center justify-between bg-teal-500 p-6">
-            {data.map(data => {
-                return (
-                    <p className="hover:bg-teal-700">{data}</p>
-                )
-            })}
+        <nav className="flex flex-col items-center justify-between bg-teal-500">
+            {data.map((data, index) => (
+                <div key={index} className="hover:bg-teal-700 p-6">
+                    <p>{data}</p>
+                </div>
+            ))}
         </nav>
-    )
+    );
 }
