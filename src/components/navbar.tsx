8fada424d70afabc5b5247937f00adf73378de6a
felipe <felipe.a.obregon@gmail.com>
2023-05-26 11:00:57 -0400

    change pages array to an object array

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,17 @@
 import Link from 'next/link'
 
 export default function Navbar() {
-    const pages = ['', 'posts', 'about'];
+    const pages = [
+        { name: 'home', url: '/' },
+        { name: 'posts', url: '/posts' },
+        { name: 'about', url: '/about' }
+    ];
 
     return (
         <nav className="flex flex-col items-center justify-between bg-teal-500">
             {pages.map((page, index) => (
                 <div key={index} className="hover:bg-teal-700 p-6">
-                    <Link href={'/' + page}>{page}</Link>
+                    <Link href={page.url}>{page.name}</Link>
                 </div>
             ))}
         </nav>
