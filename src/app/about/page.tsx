f405540fd3bf855250f454745bcdfdad245badef
felipe <felipe.a.obregon@gmail.com>
2023-05-27 18:16:33 -0400

    add profile pic

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
+import Image from 'next/image'
+
 export default function About() {
     return (
-        <h1>about</h1>
+        <>
+            <h1>about</h1>
+            <Image className="rounded-full"alt="me" src="/me.jpg" width="300" height="300"></Image>
+        </>
     )
 }
