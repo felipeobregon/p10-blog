7f0dae57ae8cc5dc9b7bfa86def36837838a2256
felipe <felipe.a.obregon@gmail.com>
2023-06-04 09:13:30 -0400

    add style to ProfileCard

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function ProfileCard() {
     return (
-        <>
+        <div className="flex flex-col items-center">
             <Image className="rounded-full"alt="me" src="/me.jpg" width="300" height="300"></Image>
-            <h1 className="text-lg">Felipe Obregon</h1>
-        </>
+            <h1 className="text-2xl">Felipe Obregon</h1>
+        </div>
     )
 }
