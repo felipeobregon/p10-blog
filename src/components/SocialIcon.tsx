14504a58882a5dbfc2433bd50353ac0fa57ff139
felipe <felipe.a.obregon@gmail.com>
2023-06-04 10:07:55 -0400

    define SocialIcon

---
+++ src/components/SocialIcon.tsx
@@ -0,0 +1,15 @@
+import TwitterIcon from './twitter.svg'
+
+export default function SocialIcon() {
+
+    const SocialImage = TwitterIcon
+    const link = "https://twitter.com"
+
+    const size = 30
+
+    return (
+        <a href={link}>
+            <SocialImage width={size} height={size} className="fill-blue-500"/>
+        </a>
+    )
+}
