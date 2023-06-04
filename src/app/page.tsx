f0496fbe274da936fba83e742bc05d6a4b70147a
felipe <felipe.a.obregon@gmail.com>
2023-06-04 08:34:30 -0400

    remove Twitter from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Twitter from '../components/twitter.svg'
 
 export default function Home() {
     return (
         <>
             <h1>hello</h1>
-            <Twitter />
         </>
 
     )
