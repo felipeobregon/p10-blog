5d8143939b489c7ebecc4244e6a720c639de674c
felipe <felipe.a.obregon@gmail.com>
2023-06-04 09:00:36 -0400

    use ProfileCard

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
+import ProfileCard from '@/components/ProfileCard'
 import Image from 'next/image'
 
 export default function About() {
     return (
         <>
-            <h1>about</h1>
-            <Image className="rounded-full"alt="me" src="/me.jpg" width="300" height="300"></Image>
+            <ProfileCard />
         </>
     )
 }
