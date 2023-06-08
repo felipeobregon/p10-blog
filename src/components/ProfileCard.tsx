5068f5dba1585a92f64e0496876e95e560573e62
felipe <felipe.a.obregon@gmail.com>
2023-06-07 21:06:28 -0400

    add width to socials bar

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -6,8 +6,8 @@ export default function ProfileCard() {
         <div className="flex flex-col items-center">
             <Image className="rounded-full" alt="me" src="/me.jpg" width="300" height="300"></Image>
             <h1 className="text-2xl font-bold">Felipe Obregon</h1>
-            <p>Software Engineer</p>
-            <div className="flex">
+            <p >Software Engineer</p>
+            <div className="flex grow w-1/4 justify-between">
                 <SocialIcon type="Twitter" />
                 <SocialIcon type="LinkedIn" />
                 <SocialIcon type="GitHub" />
